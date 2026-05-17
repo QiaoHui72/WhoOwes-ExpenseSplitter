449bf6b594d3abccb980129ec8815f710027bda6
QiaoHui72 <teohqiaohui72@gmail.com>
2026-05-17 22:11:41 +0800

    Update database.php

--- database.php
+++ database.php
@@ -6,9 +6,9 @@ $password = getenv("DB_PASS");
 $database = getenv("DB_NAME");
 $port = getenv("DB_PORT");
 
-$conn = new mysqli($host, $user, $password, $database, $port);
+$connect = new mysqli($host, $user, $password, $database, $port);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if ($connect->connect_error) {
+    die("Connection failed: " . $connect->connect_error);
 }
 ?>
