b098246f5d416a8e714b515191028b38a0827823
QiaoHui72 <teohqiaohui72@gmail.com>
2026-05-17 21:53:03 +0800

    Revert "railway deployment"
    This reverts commit 6ca5cd0fe3a773e3096bcf2d17addc5ea969c365.

--- database.php
+++ database.php
@@ -1,11 +1,11 @@
 <?php
-$host = getenv("MYSQLHOST");
-$user = getenv("MYSQLUSER");
-$password = getenv("MYSQLPASSWORD");
-$database = getenv("MYSQLDATABASE");
-$port = getenv("MYSQLPORT");
+//create a connection to the database
+$servername = "localhost"; //127.0.0.1 //computer name
+$username = "root"; //default username admin
+$password = ""; //default password is empty
+$dbname = "whoowes"; //database name
 
-$conn = new mysqli($host, $user, $password, $database, $port);
+$connect = mysqli_connect($servername, $username, $password, $dbname);
 if (!$connect) {
     die("Connection Failed: " . mysqli_connect_error());
 }
